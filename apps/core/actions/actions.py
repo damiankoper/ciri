9a07e123c1c5ac651d7408be3aa7da4fceeff26f
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-03-16 15:43:13 +0100

    feat: add local time story

--- apps/core/actions/actions.py
+++ apps/core/actions/actions.py
@@ -4,24 +4,23 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from datetime import datetime
+from typing import Any, Text, Dict, List
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionTimeDefaultLocation(Action):
+
+  def name(self) -> Text:
+    return "action_time_default_location"
+
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    response = f"Current local time is {current_time}"
+    dispatcher.utter_message(text=response)
+
+    return []
