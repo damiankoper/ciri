c94d8f08f4c68d677581d4140df06f2889cfbb1e
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-03-16 16:04:50 +0100

    feat: add all no-entity actions

--- apps/core/actions/actions.py
+++ apps/core/actions/actions.py
@@ -4,7 +4,7 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -20,7 +20,50 @@ class ActionTimeDefaultLocation(Action):
     domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
     now = datetime.now()
     current_time = now.strftime("%H:%M")
-    response = f"Current local time is {current_time}"
+    response = f"Current local time is {current_time}."
+    dispatcher.utter_message(text=response)
+
+    return []
+
+
+class ActionDayToday(Action):
+  def name(self) -> Text:
+    return "action_day_today"
+
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    day = datetime.today().strftime('%A')
+    response = f"Today is {day}."
+    dispatcher.utter_message(text=response)
+
+    return []
+
+class ActionDayTommorow(Action):
+  def name(self) -> Text:
+    return "action_day_tomorrow"
+
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    day = (datetime.today() + timedelta(days=1)).strftime('%A')
+    response = f"Tommorow will be {day}."
+    dispatcher.utter_message(text=response)
+
+    return []
+
+class ActionDateAndTime(Action):
+  def name(self) -> Text:
+    return "action_date_and_time"
+
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    day_and_time = datetime.now().strftime('%H:%M, %A %d %B %Y')
+    response = f"It is now {day_and_time}."
     dispatcher.utter_message(text=response)
 
     return []
