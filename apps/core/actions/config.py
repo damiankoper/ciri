d066809db5adb56173125ade7a308b473ebc80ca
damiankoper <kopernickk@gmail.com>
2022-05-06 00:59:49 +0200

    feat: ticker lookup with polygon api

--- apps/core/actions/config.py
+++ apps/core/actions/config.py
@@ -3,6 +3,7 @@ import os
 WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
 FINNHUB_API_KEY = os.environ.get('FINNHUB_API_KEY')
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+POLYGON_API_KEY = os.environ.get('POLYGON_API_KEY')
 
 ERROR_MESSAGE = {"type": "error",
                  "message": "Remote source error occured. Check input data"}
--- apps/core/actions/subjects/stock_actions.py
+++ apps/core/actions/subjects/stock_actions.py
@@ -1,9 +1,16 @@
+from datetime import datetime
 from typing import Any, Text, Dict, List
+from ..utils.common import create_default_json_response
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
 
-from ..config import FINNHUB_API_KEY, ERROR_MESSAGE
+from ..config import FINNHUB_API_KEY, ERROR_MESSAGE, POLYGON_API_KEY
+
+
+class TickerNotDetected(Exception):
+    pass
+
 
 class ActionStockPrice(Action):
     def name(self) -> Text:
@@ -13,21 +20,40 @@ class ActionStockPrice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         entities = tracker.latest_message['entities']
-        if len(entities) == 0:
+
+        try:
+            if len(entities) == 0:
+                raise TickerNotDetected
+
+            company_name = entities[0]['value']
+            response = requests.get(
+                f'https://api.polygon.io/v3/reference/tickers?type=CS&market=stocks&search={company_name}&active=true&limit=1&apiKey={POLYGON_API_KEY}')
+            response_json = response.json()
+
+            if response.status_code != 200 or response_json['count'] == 0:
+                raise TickerNotDetected
+
+            ticker = response_json['results'][0]['ticker']
+            name = response_json['results'][0]['name']
+            currency = response_json['results'][0]['currency_name']
+
+            response = requests.get(
+                f'https://finnhub.io/api/v1/quote?symbol={ticker}&token={FINNHUB_API_KEY}')
+            response_json = response.json()
+            print(response_json)
+
+            price = response_json['c']
+            time = datetime.fromtimestamp(int(response_json['t']))
+
+            response = create_default_json_response(
+                f"{name} {ticker} {price} {currency} {time}")
+            dispatcher.utter_message(json_message=response)
+
+            # TODO - enhance symbol lookup and fallback
+
+        except TickerNotDetected:
             dispatcher.utter_message(json_message=ERROR_MESSAGE)
-            return []
 
-        company_name = entities[0]['value'].replace(' ', '')
-        response = requests.get(
-            f'https://finnhub.io/api/v1/search?q={company_name}&token={FINNHUB_API_KEY}')
-
-        if response.status_code != 200:
-            dispatcher.utter_message(json_message=ERROR_MESSAGE)
-            return []
-
-        # TODO - fix lowercase symbols handling
-        # TODO - enhance symbol lookup
-        print(response.json()['result'][0]['symbol'])
         return []
 
 
